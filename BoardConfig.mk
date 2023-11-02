284078c1a3312ff183f48ea12487996488ed67cc
Ahmed Shahir <ashahir05@gmail.com>
2023-11-02 11:09:53 +0600

    Fix

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -73,8 +73,8 @@ BOARD_VENDORIMAGE_FILE_SYSTEM_TYPE := ext4
 TARGET_COPY_OUT_VENDOR := vendor
 BOARD_SUPER_PARTITION_SIZE := 9126805504
 BOARD_SUPER_PARTITION_GROUPS := samsung_dynamic_partitions
-BOARD_SAMSUNG_DYNAMIC_PARTITIONS_PARTITION_LIST := system vendor product odm system_ext vendor_dlkm system_dlkm
-BOARD_SAMSUNG_DYNAMIC_PARTITIONS_SIZE := 9122611200
+BOARD_MAIN_PARTITION_LIST := system vendor product odm system_ext vendor_dlkm system_dlkm
+BOARD_MAIN_SIZE := 9122611200
 
 # Platform
 TARGET_BOARD_PLATFORM := universal3830
